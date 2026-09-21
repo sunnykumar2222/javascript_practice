641b22d88cc6ffa26f25d7fa328123a970715831
Sunny Kumar <sunnymishra2222@gmail.com>
2026-09-21 17:54:39 +0000

    container cinfiguration done

---
+++ 01_basics/test.js
@@ -0,0 +1,11 @@
+console.log("sunny")
+
+const xyz = 123   // ye  constant hai, iska value change nhi ho skta hai
+let abc = "Sunny Bhai" //let 1 datatype jaisa hai 
+
+a=5555 // ye bina data type ka variable hai, jaise python me krte haiwaise hi issme v kar skte hai
+b=3333
+var c = "Apna"
+
+// agar aapko multiple variable ko 1 baar me print krna hai to aise kar skte hai
+console.table([xyz, abc, a, b, c]) 
