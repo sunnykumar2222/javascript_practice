567fd9f9a023910b0ca8570d36751605936a4f32
Sunny Kumar <sunnymishra2222@gmail.com>
2026-09-23 19:28:59 +0530

    alert statement does  not work in node.js directly, its have its special syntax

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -4,3 +4,10 @@ console.log("hello we are studying datatypes")
               // halaki ab ka jo naya compiler hai wo automatically saare code ko newer version jaisa hi treat krta hai.
 
 console.log("hello")              
+
+//alert("hello") // ye 1 statement hai jiska use ham bhout krenge, saayed jobrowser me jo alert message aata hai wo issi k wajah se aata hai
+                //                ye abhi output iss liye nhi de rha hai kuiki  yaha pe node.js k andar isko likhne ka syntax thora alag hai, wahi pe agar 
+               //                ham browser me jaa kar developer mode khol kar uske console me likhte hai to ye output de deta hai, kuiki waha pura k pura javascript ka engine hota hai
+
+//alert( 3 + 3)
+
