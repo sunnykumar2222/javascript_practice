315537346d382a75111e1cd9575feb3de6367681
Sunny Kumar <sunnymishra2222@gmail.com>
2026-09-23 19:18:41 +0530

    started studying datatypes of javascript

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,6 @@
+console.log("hello we are studying datatypes")
+
+"use strict"; // ye ham likhte hai taaki ham compiler ko bol ske ki aap ye saare javascript code ko newer version k tarah treat kare
+              // halaki ab ka jo naya compiler hai wo automatically saare code ko newer version jaisa hi treat krta hai.
+
+console.log("hello")              
